77059db382a155929b99f0dceb2dd4927dfc480a
Charan Kumar <kcharankumar@gmail.com>
2022-07-31 23:49:24 +0530

    test.java

--- test.java
+++ test.java
@@ -4,7 +4,9 @@ public class Test{
 
   public static void main (String args[]){
 
-    System.out.println ("Test...");   
+    System.out.println ("Test..."); 
+
+    //Writing Spring REST code to fetch the details...   
 
 
  }
